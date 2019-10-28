0c159810117071623f8c034ba2e3703c51c3d4c2
YhayhThabet <56704443+YhayhThabet@users.noreply.github.com>
2019-10-27 20:52:54 -0400

    Add files via upload

--- DonutShop/src/simQueue.java
+++ DonutShop/src/simQueue.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class simQueue {
 	public static Random r = new Random(97);
@@ -12,14 +8,14 @@ public class simQueue {
 	public static int aver = 0;
 	public static int inService = 0;
 	public static int completedService = 0;
+	public static int heavyLow = 0;
 
 	public static void main(String[] args) {
-
 		Queue<Customer> line = new LinkedList<Customer>();
-		ArrayList<Integer> servers = new ArrayList<Integer>();
+		LinkedList<Integer> servers = new LinkedList<Integer>();
 
 		int numServers = displayMenu(); // Displays menu and gets number of servers
-		
+
 		while (numServers > 0) { // Adds specific amount servers
 			servers.add(null);
 			numServers--;
@@ -27,38 +23,17 @@ public class simQueue {
 
 		for (int i = 0; i < 20; i++) { // Each loop is 1 tick.
 			int numCust = getPoissonRandom(2); // Number of customers
-
-			for (int j = 0; j < servers.size(); j++) { // Releases a customer when done
-				if (servers.isEmpty() || servers.get(j) == null)
-					break;
-				else if (servers.get(j) == 0) { // Remove customer and service time to servers
-					Customer c = line.remove();
-					servers.set(j, c.serviceTime);
-				}
-				servers.set(j, servers.get(j) - 1); // Decrement service timeS
-			}
-
-			for (Customer c : line) { // Adds time to remaining customers
-				c.setWaitTime(c.getWaitTime() + 1);
-			}
-
-			for (int j = 0; j < numCust; j++) { // Adds customers to line
-				line.add(new Customer(getPoissonRandom(meanT)));
-			}
-
+			
+			serversWork(servers, line);
+			addWaitTime(line);
+			addToLine(numCust, line);
+			findValues(line);
+			
 			System.out.println(line.size() + " Customers in queue:");
-
-			for (int j = 0; j < line.size(); j++) {
-				Customer c = line.remove();
-				max(c.waitTime);
-				min(c.waitTime);
-				aver += c.waitTime;
-				line.add(c);
-			}
 			System.out.println("Total Wait Time: " + aver);
 			System.out.println("Wait Time: " + minWaitTime + ", " + aver / line.size() + ", " + maxWaitTime);
 			System.out.println("======================");
-
+			decrementServerTime(servers);
 		}
 
 	}
@@ -71,10 +46,56 @@ public class simQueue {
 			p = p * r.nextDouble();
 			k++;
 		} while (p > L);
+		
 		return k - 1;
 
 	}
+	
+	public static void serversWork(LinkedList<Integer> servers, Queue<Customer> line ) {
+		for (int j = 0; j < servers.size(); j++) { // Releases a customer when done
+			if (line.isEmpty())
+				break;
+			else if (servers.get(j) == null) {
+				Customer c = line.remove();
+				servers.set(j, c.serviceTime);
+			} else if (servers.get(j) == 0) { // Remove customer and service time to servers
+				Customer c = line.remove();
+				servers.set(j, c.serviceTime);
+			}
+		}
+	}
+	
+	public static void addWaitTime(Queue<Customer> line) {
+		for (Customer c : line) { // Adds time to remaining customers
+			c.setWaitTime(c.getWaitTime() + 1);
+		}
+	}
+	
+	public static void addToLine(int numCust, Queue<Customer> line) {
+		for (int j = 0; j < numCust; j++) { // Adds customers to line
+			line.add(new Customer(r.nextInt(heavyLow)));
+		}
+	}
+	
+	public static void findValues(Queue<Customer> line) {
+		for (int j = 0; j < line.size(); j++) { // Finds min, max, average
+			Customer c = line.remove();
+			max(c.waitTime);
+			min(c.waitTime);
+			aver += c.waitTime;
+			line.add(c);
+		}
+	}
+	
+	public static void decrementServerTime(LinkedList<Integer> servers) {
+		for (int j = 0; j < servers.size(); j++) {
+			if (servers.get(j) != null) {
+				servers.set(j, servers.get(j) - 1); // Decrement service times
+			}
 
+		}
+	}
+	
 	public static int displayMenu() { // Displays menu
 		Scanner input = new Scanner(System.in);
 
@@ -84,22 +105,24 @@ public class simQueue {
 		int choice = input.nextInt();
 
 		if (choice == 1) {
+			heavyLow = 13;
 			meanT = 2;
 			return 7;
 		} else {
+			heavyLow = 4;
 			meanT = 0.25;
 			return 4;
 		}
 
 	}
 
-	public static void max(int c) {
+	public static void max(int c) { // Find Max
 		int min = c;
 		if (min > maxWaitTime)
 			maxWaitTime = min;
 	}
 
-	public static void min(int c) {
+	public static void min(int c) { // Find Min
 		int max = c;
 		if (max < minWaitTime)
 			minWaitTime = max;
